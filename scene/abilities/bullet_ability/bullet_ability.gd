c63e4b175d89fe1190d5f97d48524e91fa51c40d
Alex Volkov <aleksandr.volkov1101@gmail.com>
2024-12-29 12:28:40 +0300

    Добавлена стрельба тестовая

--- scene/abilities/bullet_ability/bullet_ability.gd
+++ scene/abilities/bullet_ability/bullet_ability.gd
@@ -1,24 +1,14 @@
 extends Node2D
 class_name BulletAbility
 
+@export var speed: float = 400
 @onready var hit_box_component: HitBoxComponent = $HitBoxComponent
 
-@export var speed: float = 500  # Скорость пули
-var direction: Vector2 = Vector2.ZERO  # Направление пули
+var direction: Vector2
 
 func _ready():
-	# Рассчитываем направление от текущей позиции до позиции мыши
-	var mouse_position = get_global_mouse_position()
-	direction = (mouse_position - global_position).normalized()
+	direction = Vector2.ZERO
 
-	# Настраиваем урон пули
-	if hit_box_component:
-		hit_box_component.damage = 10
-
-func _process(delta):
-	# Двигаем пулю в рассчитанном направлении
-	position += direction * speed * delta
-
-	# Если пуля выходит за пределы экрана, удаляем её
-	if not get_viewport().get_visible_rect().has_point(global_position):
-		queue_free()
+func _process(delta: float) -> void:
+	if direction != Vector2.ZERO:
+		global_position += direction * speed * delta
--- scene/abilities/bullet_ability/bullet_ability_controller.gd
+++ scene/abilities/bullet_ability/bullet_ability_controller.gd
@@ -1,20 +1,28 @@
 extends Node
 
-
 @export var bullet_ability_scene: PackedScene
-
 var damage = 10
 
-func _on_timer_timeout() -> void:
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		spawn_bullet()
+
+func spawn_bullet():
 	var player = get_tree().get_first_node_in_group('player') as Node2D
 	if player == null:
 		return
-		
+
 	var front_layer = get_tree().get_first_node_in_group('front_layer') as Node2D
-	
-	
 	var bullet_ability_instance = bullet_ability_scene.instantiate() as BulletAbility
 	front_layer.add_child(bullet_ability_instance)
-	
+
+	# Устанавливаем начальную позицию пули
 	bullet_ability_instance.global_position = player.global_position
+
+	# Рассчитываем направление от игрока к мыши
+	var mouse_position = get_viewport().get_mouse_position()
+	var direction = (mouse_position - player.global_position).normalized()
+
+	# Устанавливаем направление и урон
+	bullet_ability_instance.direction = direction
 	bullet_ability_instance.hit_box_component.damage = damage
