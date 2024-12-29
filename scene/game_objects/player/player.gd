edd1e3f6321afd4610e1a7c5c3430cfa9c955228
Alex Volkov <aleksandr.volkov1101@gmail.com>
2024-12-29 17:34:46 +0300

    пистолет 360 градусов

--- scene/game_objects/player/player.gd
+++ scene/game_objects/player/player.gd
@@ -6,6 +6,8 @@ extends CharacterBody2D
 @onready var ability_manager = $AbilityManager
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var bullet_ability_controller = $AbilityManager/BulletAbilityController
+@onready var gun_marker = $GunCast2D/Marker2D  # Убедитесь, что вы правильно настроили этот путь
+@onready var gun_sprite = $GunCast2D/Pistolet2D  # Пистолет (сам спрайт)
 
 var max_speed = 125
 var acceleration = .15
@@ -18,12 +20,13 @@ func _ready():
 	health_update()
 
 func _process(delta):
+	# Обработка движения персонажа
 	var direction = movement_vector().normalized()
 	var target_velocity = max_speed * direction
-	
 	velocity = velocity.lerp(target_velocity, acceleration)
 	move_and_slide()
-	
+
+	# Воспроизведение анимации в зависимости от движения
 	if direction.x != 0 || direction.y != 0:
 		animated_sprite_2d.play("run")
 	else:
@@ -33,24 +36,31 @@ func _process(delta):
 	if face_sign != 0:
 		animated_sprite_2d.scale.x = face_sign
 	
+	# Поворот пистолета в сторону мыши
+	rotate_gun_to_mouse()
+
 	# Стрельба
 	if Input.is_action_just_pressed("shoot"):
 		bullet_ability_controller.spawn_bullet()
 
+# Вычисление вектора движения
 func movement_vector():
 	var movement_x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var movement_y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return Vector2(movement_x, movement_y)
 
+# Обработка повреждений
 func check_if_damaged():
 	if enemies_colliding == 0 or not grace_period.is_stopped():
 		return
 	health_component.take_damage(1)
 	grace_period.start()
 
+# Обновление здоровья
 func health_update():
 	progress_bar.value = health_component.get_health_value()
 
+# Обработка столкновений с врагами
 func _on_player_hurt_box_area_entered(area):
 	enemies_colliding += 1
 	check_if_damaged()
@@ -58,18 +68,33 @@ func _on_player_hurt_box_area_entered(area):
 func _on_player_hurt_box_area_exited(area):
 	enemies_colliding -= 1
 
+# Функция, вызываемая при смерти игрока
 func on_died():
 	queue_free()
 
+# Обновление здоровья
 func on_health_changed():
 	health_update()
 
+# Таймаут после получения урона
 func _on_grace_period_timeout():
 	check_if_damaged()
 
+# Обработчик обновлений способностей
 func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
 	if not upgrade is NewAbility:
 		return
 		
 	var new_ability = upgrade as NewAbility
 	ability_manager.add_child(new_ability.new_ability_scene.instantiate())
+
+# Поворот пистолета в сторону мыши
+func rotate_gun_to_mouse():
+	var mouse_position = get_global_mouse_position()  # Позиция мыши в мировых координатах
+	var gun_position = gun_marker.global_position  # Позиция пистолета
+
+	# Вычисляем угол между пистолетом и мышью
+	var angle_to_mouse = gun_position.angle_to_point(mouse_position)
+
+	# Поворачиваем пистолет
+	gun_sprite.rotation = angle_to_mouse
