f501f4117028c2a9f00b0c9f7784c11150631e02
Alex Volkov <aleksandr.volkov1101@gmail.com>
2025-01-27 19:12:29 +0300

    added menu and range enemy

---
+++ scene/ui/menu.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+func _on_texture_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/level/level.tscn")
+
+
+func _on_texture_quit_pressed() -> void:
+	get_tree().quit()
