86c2046293d53699535d13ddb367ee52226cb0c9
Alex Volkov <aleksandr.volkov1101@gmail.com>
2025-02-08 02:54:45 +0300

    Добавлена тестовая сцена с талантами

--- scene/ui/menu.gd
+++ scene/ui/menu.gd
@@ -8,3 +8,7 @@ func _on_texture_play_pressed() -> void:
 
 func _on_texture_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_texture_talents_pressed() -> void:
+	get_tree().change_scene_to_file('res://scene/ui/tallants/talents_menu.tscn')
