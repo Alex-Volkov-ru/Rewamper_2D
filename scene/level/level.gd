f9427df87ed2186a8ef2268b4e2613cf858c2dc8
Alex Volkov <aleksandr.volkov1101@gmail.com>
2025-02-13 10:58:34 +0300

    фикс монеты в игровой сессии, обнуление после каждого запуска уровня

--- scene/level/level.gd
+++ scene/level/level.gd
@@ -4,11 +4,10 @@ extends Node
 
 @onready var player = %Player
 
-
 func _ready():
 	player.health_component.died.connect(on_died)
-	
-	
+	Global.coin_manager.clear_current_coins()  # Очищаем монеты перед стартом уровня
+
 func on_died():
 	var end_screen_instance = end_screen_scene.instantiate() as EndScreen
 	add_child(end_screen_instance)
--- scene/manager/coin_manager.gd
+++ scene/manager/coin_manager.gd
@@ -9,20 +9,13 @@ func _ready():
 	# Подключаем сигнал, если он еще не подключен
 	if not Global.coin_collected.is_connected(on_coin_collected):
 		Global.coin_collected.connect(on_coin_collected)
-	
-	# Логируем, чтобы убедиться, что CoinManager загружен
-	print("CoinManager готов")
 
 # Обработчик сбора монеты
 func on_coin_collected(coin: int):
-	print("Собрана монета:", coin)  # Логируем, сколько монет собрано
 	current_coins += coin  # Увеличиваем количество монет
-	print("Текущее количество монет:", current_coins)  # Логируем текущие монеты
 	coins_updated.emit(current_coins)  # Отправляем обновленное количество монет
 
 # Метод для очистки монет (сброс текущего количества монет)
 func clear_current_coins():
-	print("Монеты очищаются!")  # Логируем этот момент
 	current_coins = 0  # Обнуляем текущее количество монет
-	print("Текущее количество монет после очистки:", current_coins)  # Логируем после очистки
 	coins_updated.emit(current_coins)  # Отправляем обновленное количество монет
