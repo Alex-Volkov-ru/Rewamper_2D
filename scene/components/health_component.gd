1a550179ab0f238ca005a4c526e3418eb60f7657
Alex Volkov <aleksandr.volkov1101@gmail.com>
2025-02-21 12:40:27 +0300

    leaderboard

--- scene/components/health_component.gd
+++ scene/components/health_component.gd
@@ -36,4 +36,3 @@ func check_death():
 		
 		# Проверяем, существует ли ключ "kill_mobs_count" в save_data
 		Save_Manager_Progress.kill_mobs_count +=1
-		print("Убито мобов: ", Save_Manager_Progress.kill_mobs_count)
--- scene/ui/menu.gd
+++ scene/ui/menu.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 
+
+
+
+
 func _on_texture_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/level/level.tscn")
 
@@ -11,3 +15,21 @@ func _on_texture_talents_pressed() -> void:
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file('res://scene/ui/settings/settings.tscn')
+
+
+func _on_lead_board_pressed() -> void:
+	var options
+
+	match Bridge.platform.id:
+		"y8":
+			options = {
+				"table": "leader"
+			}
+
+	Bridge.leaderboard.show_native_popup(options, Callable(self, "_on_show_native_popup_completed"))
+
+func _on_show_native_popup_completed(success):
+	if success:
+		print("Таблица лидеров успешно открыта.")
+	else:
+		print("Ошибка при открытии таблицы лидеров.")
